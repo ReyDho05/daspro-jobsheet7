42164c84eb88a1fd27bb7773a345405165be2ec8
ReyDho05 <rzulnisfian>
2024-10-08 12:32:35 +0700

    modified

--- SiakadFor06.java
+++ SiakadFor06.java
@@ -1,12 +1,10 @@
 import java.util.Scanner;
-/**
- * SiakadFor06
- */
 public class SiakadFor06 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double nilai, tertinggi=0, terendah=100;
+        int lulus=0,tdklulus=0;
         for(int i =1; i<=10; i++){
             System.out.print("Masukkan nilai mahasiswa ke-"+i+": ");
             nilai=sc.nextDouble();
@@ -16,7 +14,15 @@ public class SiakadFor06 {
             if (nilai<terendah) {
                 terendah = nilai;
             } 
-        } System.out.println("Nilai tertinggi : "+tertinggi);
-          System.out.println("Nilai terendah : "+terendah);
+            if (nilai >= 60) {
+                lulus++;
+            } else {
+                tdklulus++;
+            }
+        } 
+        System.out.println("Nilai tertinggi : "+tertinggi);
+        System.out.println("Nilai terendah : "+terendah);
+        System.out.println("Jumlah mahasiswa yang lulus: " + lulus);
+        System.out.println("Jumlah mahasiswa yang tidak lulus: " + tdklulus);
     }
 }
